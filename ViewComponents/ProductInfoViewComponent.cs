baecae702ea303a5d79f8bd4a6320b144d47a843
nguyenduchuy19012004 <zzcz4991@gmail.com>
2026-07-14 14:41:46 +0700

    feat: add ProductInfo ViewComponent

---
+++ ViewComponents/ProductInfoViewComponent.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.ViewComponents
+{
+    public class ProductInfoViewComponent : ViewComponent
+    {
+        private readonly StoreDbContext _context;
+
+        public ProductInfoViewComponent(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(long productId)
+        {
+            var product = await _context.Products
+                .Include(p => p.Images)
+                .Include(p => p.Reviews)
+                .FirstOrDefaultAsync(p => p.ProductID == productId);
+
+            return View(product);
+        }
+    }
+}
